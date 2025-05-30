cf0cc1564703831ebae6583bbe2abefa243f1cb0
ROK <roking2000@konkuk.ac.kr>
2025-05-30 20:46:34 +0900

    #3 [feat] : 지도 띄우기, 기관찾기 UI 구현

--- app/src/main/java/com/konkuk/gomgomee/presentation/findcare/FindCareScreen.kt
+++ app/src/main/java/com/konkuk/gomgomee/presentation/findcare/FindCareScreen.kt
@@ -1,13 +1,32 @@
 package com.konkuk.gomgomee.presentation.findcare
 
+import android.graphics.fonts.Font
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.konkuk.gomgomee.R
 import com.konkuk.gomgomee.ui.theme.White
 import com.naver.maps.geometry.LatLng
 import com.naver.maps.map.CameraPosition
@@ -28,28 +47,59 @@ fun FindCareScreen(
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.Center
     ) {
-        Text(
-            text = "기관 찾기 메인 화면"
-        )
-        NaverMapScreen()
+        //상단 배너
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(150.dp)
+                .padding(horizontal = 22.dp), // 좌우 여백
+            verticalAlignment = Alignment.Bottom
+        ) {
+            Text(
+                text = "곰곰이랑\n병원 · 심리센터 찾아보기 !",
+                fontSize = 23.sp,
+                color = Color.Black,
+                fontWeight = FontWeight.Bold
+            )
+
+            Image(
+                painter = painterResource(id = R.drawable.ic_bear_findcare),
+                contentDescription = "곰돌이 캐릭터",
+                contentScale = ContentScale.Fit,
+                modifier = Modifier
+                    .size(100.dp)
+                    .clip(RoundedCornerShape(12.dp))
+            )
+        }
+        Spacer(modifier = Modifier.height(10.dp))
+        //지도
+        Box(modifier = Modifier.weight(1f)) {
+            NaverMapScreen()
+        }
     }
 }
 
 @OptIn(ExperimentalNaverMapApi::class)
 @Composable
 fun NaverMapScreen(modifier: Modifier = Modifier) {
-    val konkuk = LatLng(37.5408,127.0793)
-    val cameraPositionState = rememberCameraPositionState{
-        position = CameraPosition(konkuk,15.0)
+    val konkuk = LatLng(37.5408, 127.0793)
+    val cameraPositionState = rememberCameraPositionState {
+        position = CameraPosition(konkuk, 15.0)
     }
 
     NaverMap(
-        modifier=modifier.fillMaxSize(),
+        modifier = modifier.fillMaxSize(),
         cameraPositionState = cameraPositionState
-    ){
+    ) {
         Marker(
             state = rememberMarkerState(position = konkuk),
             captionText = "건국대학교"
         )
     }
+}
+
+@Preview
+@Composable
+private fun PrevFindCareScreen() {
+    FindCareScreen()
 }
