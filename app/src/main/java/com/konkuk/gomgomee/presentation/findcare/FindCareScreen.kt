5e5fe73a0dfaf71e030c0f5cc6a3e8eebd9cfbd4
ROK <roking2000@konkuk.ac.kr>
2025-06-07 16:27:44 +0900

    [feat] : 맵 초기위치를 사용자 현재 위치로 설정

--- app/src/main/java/com/konkuk/gomgomee/presentation/findcare/FindCareScreen.kt
+++ app/src/main/java/com/konkuk/gomgomee/presentation/findcare/FindCareScreen.kt
@@ -1,6 +1,5 @@
 package com.konkuk.gomgomee.presentation.findcare
 
-import android.graphics.fonts.Font
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
@@ -16,6 +15,9 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -26,6 +28,7 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.lifecycle.viewmodel.compose.viewModel
 import com.konkuk.gomgomee.R
 import com.konkuk.gomgomee.ui.theme.White
 import com.naver.maps.geometry.LatLng
@@ -48,58 +51,13 @@ fun FindCareScreen(
         verticalArrangement = Arrangement.Center
     ) {
         //상단 배너
-        Row(
-            modifier = Modifier
-                .fillMaxWidth()
-                .height(150.dp)
-                .padding(horizontal = 22.dp), // 좌우 여백
-            verticalAlignment = Alignment.Bottom
-        ) {
-            Text(
-                text = "곰곰이랑\n병원 · 심리센터 찾아보기 !",
-                fontSize = 23.sp,
-                color = Color.Black,
-                fontWeight = FontWeight.Bold
-            )
+        UpperScreen()
 
-            Image(
-                painter = painterResource(id = R.drawable.ic_bear_findcare),
-                contentDescription = "곰돌이 캐릭터",
-                contentScale = ContentScale.Fit,
-                modifier = Modifier
-                    .size(100.dp)
-                    .clip(RoundedCornerShape(12.dp))
-            )
-        }
         Spacer(modifier = Modifier.height(10.dp))
+
         //지도
         Box(modifier = Modifier.weight(1f)) {
             NaverMapScreen()
         }
     }
-}
-
-@OptIn(ExperimentalNaverMapApi::class)
-@Composable
-fun NaverMapScreen(modifier: Modifier = Modifier) {
-    val konkuk = LatLng(37.5408, 127.0793)
-    val cameraPositionState = rememberCameraPositionState {
-        position = CameraPosition(konkuk, 15.0)
-    }
-
-    NaverMap(
-        modifier = modifier.fillMaxSize(),
-        cameraPositionState = cameraPositionState
-    ) {
-        Marker(
-            state = rememberMarkerState(position = konkuk),
-            captionText = "건국대학교"
-        )
-    }
-}
-
-@Preview
-@Composable
-private fun PrevFindCareScreen() {
-    FindCareScreen()
 }
